8eb08b066c2e4873a2f904aa9a64c38a8a3abc4d
BatPickle <56698568+BatPickle@users.noreply.github.com>
2025-03-02 00:02:13 -0500

    Add files via upload
    updated files to fix centering

---
+++ script.js
@@ -0,0 +1,24 @@
+const calcDisplay = document.getElementById("calcDisplay");
+ 
+const calcBtnSeven = document.getElementById("calcBtnSeven");
+const calcBtnEight = document.getElementById("calcBtnEight");
+const calcBtnNine = document.getElementById("calcBtnNine");
+const calcBtnFour = document.getElementById("calcBtnFour");
+const calcBtnFive = document.getElementById("calcBtnFive");
+const calcBtnSix = document.getElementById("calcBtnSix");
+const calcBtnOne = document.getElementById("calcBtnOne");
+const calcBtnTwo = document.getElementById("calcBtnTwo");
+const calcBtnThree = document.getElementById("calcBtnThree");
+const calcBtnZero = document.getElementById("calcBtnZero");
+const calcBtnDecimal = document.getElementById("calcBtnDecimal");
+
+const calcBtnDivision = document.getElementById("calcBtnDivision");
+const calcBtnMultiply = document.getElementById("calcBtnMultiply");
+const calcBtnSubtract = document.getElementById("calcBtnSubtract");
+const calcBtnAdd = document.getElementById("calcBtnAdd");
+
+const calcBtnClear = document.getElementById("calcBtnClear");
+const calcBtnEquals = document.getElementById("calcBtnEquals");
+
+
+
